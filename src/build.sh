50ac9147369202ddcb9559cce7f892ccffc8305f
CI Monk <lpsm-dev@protonmail.com>
2022-01-08 05:02:31 +0000

    fix: rename cmake flags name

--- src/build.sh
+++ src/build.sh
@@ -3,16 +3,16 @@
 export ARCH=`uname -m`
 export XMRIG_VERSION="v6.15.3"
 export XMRIG_URL="https://github.com/xmrig/xmrig.git"
-export XMRIG_BUILD_ARGS="-DXMRIG_DEPS=scripts/deps -DBUILD_STATIC=ON"
+export CMAKE_FLAGS="-DXMRIG_DEPS=scripts/deps -DCMAKE_BUILD_TYPE=Release -DWITH_OPENCL=OFF"
 
 if [[ "$ARCH" == *"aarch64"* ]]; then 
   git clone --single-branch --depth 1 --branch=$XMRIG_VERSION $XMRIG_URL
   mkdir ./xmrig/build 
   sed -i "s/kDefaultDonateLevel = 1;/kDefaultDonateLevel = 0;/g" ./xmrig/src/donate.h 
   sed -i "s/kMinimumDonateLevel = 1;/kMinimumDonateLevel = 0;/g" ./xmrig/src/donate.h 
-  XMRIG_BUILD_ARGS="$XMRIG_BUILD_ARGS -DWITH_HWLOC=OFF -DWITH_OPENCL=OFF -DCMAKE_SYSTEM_PROCESSOR=arm"
+  CMAKE_FLAGS="$CMAKE_FLAGS -DWITH_HWLOC=OFF -DCMAKE_SYSTEM_PROCESSOR=arm"
   cd xmrig/scripts && ./build_deps.sh && cd ../build 
-  cmake .. $XMRIG_BUILD_ARGS && \
+  cmake .. $CMAKE_FLAGS && \
   make -j$(nproc)
 else 
   git clone --single-branch --depth 1 --branch=$XMRIG_VERSION $XMRIG_URL
@@ -20,6 +20,6 @@ else
   sed -i "s/kDefaultDonateLevel = 1;/kDefaultDonateLevel = 0;/g" ./xmrig/src/donate.h 
   sed -i "s/kMinimumDonateLevel = 1;/kMinimumDonateLevel = 0;/g" ./xmrig/src/donate.h 
   cd xmrig/scripts && ./build_deps.sh && cd ../build 
-  cmake .. $XMRIG_BUILD_ARGS && \
+  cmake .. $CMAKE_FLAGS && \
   make -j$(nproc)
 fi
